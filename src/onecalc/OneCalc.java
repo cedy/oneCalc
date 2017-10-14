a572c7c56efb3cdbb89195f3b7ba0d3af0293c68
Yurii Lesh <cedy.ua@gmail.com>
2017-10-14 15:54:45 -0400

    Working release alpha v. 0.1; Total time spend so far ~5hr

--- src/onecalc/OneCalc.java
+++ src/onecalc/OneCalc.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package onecalc;
 
-import java.util.ArrayList;
-import java.util.List;
+package onecalc;
 
 /**
  *
@@ -18,23 +11,10 @@ public class OneCalc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String exmp = "1+2*3*4/3-(-0.5)*10+5"; // equals -1
-        String text = "";
-        List<String> expressionList = StringCalc.parseCharArrayToStringList(exmp.toCharArray());
-        StringCalc.calculateMultiplication(expressionList);
-        StringCalc.calculateDivision(expressionList);
-//        StringCalc.calculateAddition(expressionList);
-//        StringCalc.calculateSubtraction(expressionList);
-        for(int i = 0; i < expressionList.size();i++) {
-           System.out.println(expressionList.get(i));
-        }
-//        
-//        double n1 = -10;
-//        double n2 = -3;
-//        System.out.println(n1*n2);
-//        System.out.println(n1/n2);
-//        System.out.println(n1-n2);
-//        System.out.println(n1+n2);
+        String exmp = "2^(2+2)+2*2-(-5)/5"; // equals 21
+        double result = StringCalc.calculate(exmp);
+        System.out.println(result);
+
     }
     
 }
--- src/onecalc/StringCalc.java
+++ src/onecalc/StringCalc.java
@@ -14,27 +14,68 @@ public class StringCalc {
     }
     
     public static double calculate(String expression) {
+        // converts string to list of string's elements
         List<String> expressionList = StringCalc.parseCharArrayToStringList(expression.toCharArray());
-        
-        return 0;
+        // calculates expression inside parentheses
+        StringCalc.calculateParentheses(expressionList);
+        // final calculation after all parentheses are calculated
+        String result = StringCalc.calculate(expressionList);
+        return Double.parseDouble(result);
     }
     
-    protected static  char[] calculateParentheses(char[] expressionArray) {
-        for (int leftCounter = expressionArray.length-1; leftCounter == 0; leftCounter--){
-            if(expressionArray[leftCounter] =='(') {
-                expressionArray[leftCounter] = '#';
-                for(int rightCounter = leftCounter; rightCounter > expressionArray.length; rightCounter++){
-                    // call calculate method here
-                    if(expressionArray[rightCounter] ==')') {
-                        break;
-                    }
+    protected static  void calculateParentheses(List<String> expressionList) {
+        //method to calculate expression in parentheses
+            for(int rightP = 0; rightP < expressionList.size(); rightP++) {
+                //loop through list until found ')'
+                if( expressionList.get(rightP).equals(")")) {
+                     for(int leftP = rightP; leftP >= 0 ; leftP--) {
+                         //loop backwards until found matching '('
+                           if( expressionList.get(leftP).equals("(")) {
+                               //calculate expression inside parentheses
+                               String result = StringCalc.calculate(expressionList.subList(leftP+1, rightP));
+ 
+                               //remove parentheses around inner expression
+                               expressionList.remove(leftP+2);
+                               expressionList.remove(leftP);
+                               rightP = 0;                            
+                           }
+                     }
                 }
             }
-        }
-        return expressionArray;
+
+    }
+    
+    private static String calculate(List<String> expressionList) {
+        //calculating expression in mathematical order. Method returns result of the expression.
+        StringCalc.calculatePower(expressionList);
+        StringCalc.calculateMultiplication(expressionList);
+        StringCalc.calculateDivision(expressionList);
+        StringCalc.calculateAddition(expressionList);
+        StringCalc.calculateSubtraction(expressionList);
+        
+        return expressionList.get(0);
+    }
+    
+    protected static void calculatePower(List<String> expressionList) {
+        // calculates power and substitutes expression with a result
+        boolean calculationPerformed;
+        do {
+            calculationPerformed = false;
+            for(int i = 0; i < expressionList.size();i++) {
+                if( expressionList.get(i).equals("^")) {
+                    double left = Double.parseDouble(expressionList.get(i-1));
+                    double right = Double.parseDouble(expressionList.get(i+1));
+                    expressionList.set(i, String.valueOf(Math.pow(left, right)));
+                    expressionList.remove(i+1);
+                    expressionList.remove(i-1);
+                    calculationPerformed = true;
+                }
+            }
+        } while (calculationPerformed);
     }
     
     protected static void calculateMultiplication(List<String> expressionList) {
+        // calculates multiplication and substitutes expression with a result
         boolean calculationPerformed;
         do {
             calculationPerformed = false;
@@ -52,6 +93,7 @@ public class StringCalc {
     }
     
     protected static void calculateDivision(List<String> expressionList) throws ArithmeticException {
+        // calculates division and substitutes expression with a result
         boolean calculationPerformed;
         do {
             calculationPerformed = false;
@@ -70,6 +112,7 @@ public class StringCalc {
     }
     
     protected static void calculateAddition(List<String> expressionList) {
+        // calculates addition and substitutes expression with a result
         boolean calculationPerformed;
         do {
             calculationPerformed = false;
@@ -87,6 +130,7 @@ public class StringCalc {
     }
     
     protected static void calculateSubtraction(List<String> expressionList) {
+        // calculates subtraction and substitutes expression with a result
         boolean calculationPerformed;
         do {
             calculationPerformed = false;
@@ -106,6 +150,7 @@ public class StringCalc {
     protected static List<String> parseCharArrayToStringList(char[] expressionArray) {
         //expression should be checked on mathematical correctness before calling this method.
         //only number and mathematical functions such as +,-, *, /, ^ and ( )
+        // returns list of string where each element a number or mathematical function or parentheses
         List<String> expressionList = new ArrayList<>();
         for (int counter = 0; counter < expressionArray.length; counter++) {
             //parsing numbers to numbers, actions to actions
@@ -122,13 +167,16 @@ public class StringCalc {
                                break;
                  case '/':   expressionList.add("/");
                                break;
+                 case '^':   expressionList.add("^");
+                               break;              
            
                 default: String number = "";
                     for(; counter < expressionArray.length; counter++) {
-                        // check if it is a digit or a point(.)
+                        // checks if it is a digit or a point and compiles number from digits and point
                         if((expressionArray[counter] > 47 && expressionArray[counter] < 58) || expressionArray[counter] == '.') {
                             number += expressionArray[counter];
                         } else {
+                           //stops when non digit or a point goes next. puts array pointer counter in a right position
                             counter--;
                             break;
                         }
@@ -137,6 +185,7 @@ public class StringCalc {
                     break;
             }
         }
+        //negative numbers in expression should be surrounded by parentheses 
         //distinguishing negative numbers, removing parentheses around them
         for(int i = 0; i < expressionList.size();i++) {
            if (expressionList.get(i).equals("-") && expressionList.get(i-1).equals("(") && expressionList.get(i+2).equals(")")) {
@@ -151,22 +200,4 @@ public class StringCalc {
         return expressionList;
     }
     
-    private static boolean isNumber(String value) {
-        
-        if( value.length() > 1) {
-            return true;
-        }
-        switch(value.charAt(0)) {
-                 case '(':   return false;
-                 case ')':   return false;
-                 case '+':   return false;
-                 case '-':   return false;
-                 case '*':   return false;
-                 case '/':   return false;
-                 case '^':   return false;
-           
-                 default: return true;
-        }
-    }
-
 }
